a6ed4663620c8438a3394fa04dcf4064a581a813
you tae hee <f_hyang@naver.com>
2025-02-04 13:25:50 +0900

    feat: git init

---
+++ .gitmodules.js
@@ -0,0 +1,6 @@
+[submodule "realtime-citus-backend"]
+	path = realtime-backend
+	url = git@github.com:YOUNTAEHEE/realtime-citus-backend.git
+[submodule "realtime-citus-frontend"]
+	path = realtime-frontend
+	url = git@github.com:YOUNTAEHEE/realtime-citus.git
